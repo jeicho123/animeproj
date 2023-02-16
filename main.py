d09d02d092354dc5da08ae29d55bf5417d9195e1
Jei <jeichenho@gmail.com>
2023-02-15 20:24:08 -0500

    updates

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import requests
-import utils
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request
+
+with open('char_id.json', 'r') as f:
+    cache = json.load(f)
 
 app = Flask(__name__)
 
-@app.route('/')
-def get_anime_character_image():
-    response = requests.get("https://api.jikan.moe/v4/characters/117909/pictures")
-    search_results = response.json()
-    image_url = search_results["data"][0]["jpg"]["image_url"]
-    return render_template('index.html', image_url=image_url)
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    char_name = "Spike"
+
+    if request.method == 'POST':
+        char_name = request.form['search_query']
+
+    anichan_response = requests.get("https://animechan.vercel.app/api/random/character?name=" + char_name)
+    parsed_quote = anichan_response.json()
+    quote = parsed_quote["quote"] 
+    char_name = parsed_quote["character"]  
+    id = cache[char_name]
+
+    jikan_response = requests.get("https://api.jikan.moe/v4/characters/" + str(id) + "/pictures")
+    parsed_img = jikan_response.json()
+    image_url = parsed_img["data"][0]["jpg"]["image_url"]
+
+    return render_template('index.html', image_url=image_url, quote=quote, char_name=char_name)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -1,9 +1,17 @@
 import requests
+import json
+import time
 
-
-response = requests.get("https://api.jikan.moe/v4/characters").json()
+with open('character_cache.json', 'r') as f:
+    cache = json.load(f)
 
 char_id = {}
 
-for char in response["data"]:
-    char_id[char["name"]] = char["mal_id"]
+
+for id in cache["ids"][:10]:
+    response = requests.get(f"https://api.jikan.moe/v4/characters/{id}").json()
+    char_id[response["data"]["name"]] = id
+    time.sleep(2)
+
+with open('char_id.json', 'w') as f:
+    json.dump(char_id, f)
