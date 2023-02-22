42d68a994a917bf2c2f6a1bff1c99a3669a16014
Jei <jeichenho@gmail.com>
2023-02-22 17:48:11 -0500

    update

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests
 import mysql.connector
 from flask import Flask, render_template, request
 
-
 db = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -30,20 +29,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    char_name = "Spike"
+    global char_name, quote
 
     if request.method == 'POST':
-        char_name = request.form['search_query']
-
-    anichan_response = requests.get("https://animechan.vercel.app/api/random/character?name=" + char_name)
-    parsed_quote = anichan_response.json()
-    quote = parsed_quote["quote"] 
-    char_name = parsed_quote["character"]  
-    id = cache[char_name]
-
-    jikan_response = requests.get("https://api.jikan.moe/v4/characters/" + str(id) + "/pictures")
-    parsed_img = jikan_response.json()
-    image_url = parsed_img["data"][0]["jpg"]["image_url"]
+        post = request.form['search_query']    
+        if anichan_request(post):
+            char_name, quote = [*anichan_request(post)]
+     
+    mycursor.execute(f"SELECT img FROM Jikan WHERE name = '{char_name}'")
+    image_url = mycursor.fetchone()[0]
 
     return render_template('index.html', image_url=image_url, quote=quote, char_name=char_name)
 
